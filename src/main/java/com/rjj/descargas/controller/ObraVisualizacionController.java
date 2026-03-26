25b0fe1074d5c5a2348dabad01c23d0dd78695d1
JosPapiRey7 <joschido4@gmail.com>
2026-03-26 14:37:26 -0600

    HistorialVisualizaciones

--- src/main/java/com/rjj/descargas/controller/ObraVisualizacionController.java
+++ src/main/java/com/rjj/descargas/controller/ObraVisualizacionController.java
@@ -1,9 +1,12 @@
 package com.rjj.descargas.controller;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import com.rjj.descargas.controller.dto.RVisualizacion;
+import com.rjj.descargas.entity.ObraVisualizacion;
 import com.rjj.descargas.service.ObraVisualizacionService;
 
 @RestController
@@ -18,4 +21,11 @@ public class ObraVisualizacionController {
     service.registrarVisualizacion(visualizacion);
     return "Visualizacion registrada";
   }
+
+  // En ObraVisualizacionController.java
+ @GetMapping("/{obraId}")
+public List<ObraVisualizacion> obtenerVisualizaciones(@PathVariable String obraId) {
+    // Cambia "repository" por "service"
+    return service.obtenerPorObra(obraId); 
+}
 }
